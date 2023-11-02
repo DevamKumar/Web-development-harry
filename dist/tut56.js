0f1335b7047765559d1c7ba6fe0a2651a26d5bef
Devam Kumar <121247156+DevamKumar@users.noreply.github.com>
2023-11-03 00:43:29 +0530

    new repo

---
+++ dist/tut56.js
@@ -0,0 +1,32 @@
+"use strict";
+const btn = document.getElementById("btn");
+const para = document.getElementById("para");
+let span = document.getElementById("content");
+const button = document.getElementById("btn");
+function toggleHide() {
+    if (para) {
+        if (para.style.display === 'none' || para.style.display === '') {
+            para.style.display = 'block';
+            if (span) {
+                span.innerHTML = 'Click to Hide';
+            }
+            if (button) {
+                button.innerHTML = 'Hide';
+            }
+        }
+        else {
+            para.style.display = 'none';
+            if (span) {
+                span.innerHTML = 'Click to Show';
+            }
+            if (button) {
+                button.innerHTML = 'Show';
+            }
+        }
+    }
+    else {
+        console.log("Element 'para' doesn't exist");
+    }
+}
+toggleHide();
+//# sourceMappingURL=tut56.js.map
